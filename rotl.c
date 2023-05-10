8acf15700cf4f63d9fda0b4553f234547de9cfd5
samdaphbynet <lj8@hotmail.fr>
2023-05-10 21:26:41 +0200

    function that rotates the stack to the top.

---
+++ rotl.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+/**
+ * rotl - function that rotates the stack to the top.
+ * @top: head of structure 
+ * @line: number of line
+ */
+
+void rotl(stack_t **top, unsigned int line)
+{
+	stack_t *tmp = *top, *input;
+	(void)line;
+
+	if (*top == NULL || (*top)->next == NULL)
+		return;
+	input = (*top)->next;
+	(*top)->next = NULL;
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *top;
+	(*top)->next = NULL;
+	(*top)->prev = tmp;
+	*top = input;
+}
